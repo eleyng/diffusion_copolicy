e799333cf5f2dddf6e553c4d2e8ff1f145dc36a6
Eley Ng <eleyng@stanford.edu>
2024-01-08 17:29:25 -0600

    update description;

--- zarr_read.py
+++ zarr_read.py
@@ -1,8 +1,11 @@
+'''
+This script prints the length of the dataset in zarr format.
+'''
+
 import zarr
 import os
 
-# zarr_path = "data/block_pushing/multimodal_push_seed.zarr"
-zarr_path = "data/table/table_test.zarr"
+zarr_path = "data/table/table_10Hz.zarr"
 zarr_file = zarr.open(zarr_path, mode="r")
 
 group = zarr.open(os.path.expanduser(zarr_path), "r")
